a85e14d2ecf5437c01efa5cdd15981c53e21831f
Gerson <gersonmigueis10@gmail.com>
2021-04-29 01:25:50 -0400

    [UPDATE] - Função para salvar conta na camada de serviços.

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -9,5 +9,14 @@ class AccountService {
       throw new Error(error.message);
     }
   }
+
+  static async save(name) {
+    try {
+      const dataSave = await AccountRepository.createAccounts(name);
+      return dataSave;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 module.exports = AccountService;
