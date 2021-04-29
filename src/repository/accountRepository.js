cbbc06d3a2fe19f894e3e4991cf205ed63041e7c
Gerson <gersonmigueis10@gmail.com>
2021-04-29 01:25:02 -0400

    [UPDATE] - Função e rota para salvar conta

--- src/repository/accountRepository.js
+++ src/repository/accountRepository.js
@@ -1,11 +1,19 @@
+// eslint-disable-next-line import/newline-after-import
 const database = require('../models');
-
 class AccountRepository {
   static async findAccount() {
     try {
       return await database.Accounts.findAll();
     } catch (error) {
       throw new Error('Não foi possivel listar contas');
+    }
+  }
+
+  static async createAccounts(name) {
+    try {
+      return await database.Accounts.create({ name });
+    } catch (error) {
+      throw new Error('Erro ao salvar conta');
     }
   }
 }
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -4,5 +4,6 @@ const router = Router();
 const AccountsController = require('../controllers/accountController');
 
 router.get('/accounts', AccountsController.listAccount);
+router.post('/accounts/save', AccountsController.saveAccounts);
 
 module.exports = router;
